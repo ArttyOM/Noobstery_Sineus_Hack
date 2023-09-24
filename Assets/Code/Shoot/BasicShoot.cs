3e8219a3f0b00d8d406178ebae76fc6a46163d2c
Bibijyzi <145682955+Bibijyzi@users.noreply.github.com>
2023-09-24 14:50:28 +0700

    Menu and blocks

--- Assets/Code/Shoot/BasicShoot.cs
+++ Assets/Code/Shoot/BasicShoot.cs
@@ -6,6 +6,7 @@ using System.Threading;
 using System.Collections.Generic;
 using UniRx;
 using UnityEngine.Serialization;
+using Code.Enemy;
 
 public class BulletSpawner : MonoBehaviour, IDisposable
 {
