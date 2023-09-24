8088a9deabe1e9fcb796b5e5c8a8cd9137f48c33
ArttyOM <levsha.elody@gmail.com>
2023-09-24 10:13:07 +0300

    Добавил логику GameAnalytic

--- Assets/Code/Enemy/Shooter.cs
+++ Assets/Code/Enemy/Shooter.cs
@@ -3,9 +3,8 @@ using System.Threading;
 using Code.DebugTools.Logger;
 using Cysharp.Threading.Tasks;
 using UniRx;
-using UnityEditor.U2D.Aseprite;
 using UnityEngine;
-using Random = UnityEngine.Random;
+
 
 namespace Code.Enemy
 {
