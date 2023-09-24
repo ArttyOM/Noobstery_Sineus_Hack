c9188d2863c6d2231c61ea5271f768d89f093f13
maksim <maksimkka@icloud.com>
2023-09-24 14:12:19 +0400

    Add timer

---
+++ Assets/Code/HUD/GameTimer/Timer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TMPro;
+using UnityEngine;
+
+namespace Code.HUD.GameTimer
+{
+    public class Timer : MonoBehaviour
+    {
+        [field: SerializeField] public List<TextMeshPro> TimerTexts { get; private set; }
+        private StringBuilder _stringBuilder;
+        private float _currentTime;
+        private void Awake()
+        {
+            TimerTexts = new List<TextMeshPro>();
+            _stringBuilder = new StringBuilder();
+        }
+
+        private void Update()
+        {
+            Countdown();
+        }
+
+        private void Countdown()
+        {
+                            
+            _currentTime -= Time.deltaTime;
+
+            int seconds = Mathf.FloorToInt(_currentTime);
+            foreach (var timerText in TimerTexts)
+            {
+                _stringBuilder.Clear();
+                _stringBuilder.Append(seconds);
+                timerText.text = _stringBuilder.ToString();
+            }
+            
+        }
+    }
+}
